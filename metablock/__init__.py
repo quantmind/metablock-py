09f2a958407953d2a5b61bc9e6f96005b6cf2fa9
Luca <luca@quantmind.com>
2025-02-02 13:18:15 +0000

    Drop 3.10

--- metablock/__init__.py
+++ metablock/__init__.py
@@ -5,7 +5,7 @@ from .orgs import Org
 from .spaces import Block, Service, Space, SpaceExtension
 from .user import User
 
-__version__ = "0.7.1"
+__version__ = "0.9.0"
 
 __all__ = [
     "Metablock",
